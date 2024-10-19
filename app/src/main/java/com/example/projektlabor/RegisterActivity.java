ef03b5d4d14a3f3195c0ac6605b694633ad2776b
Mathe1005 <mtvalcz@gmail.com>
2024-10-19 16:25:06 +0200

    formázás

--- app/src/main/java/com/example/projektlabor/RegisterActivity.java
+++ app/src/main/java/com/example/projektlabor/RegisterActivity.java
@@ -5,17 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -26,11 +23,11 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class RegisterActivity extends AppCompatActivity {
-    EditText usernameEditText, passwordEditText, passwordRepeatEditText;
-    Button signUpButton;
-    TextView loginText;
-    FirebaseAuth mAuth;
-    DatabaseReference mDatabase;
+    private TextInputEditText usernameEditText, passwordEditText, passwordRepeatEditText;
+    private Button signUpButton;
+    private TextView loginText;
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,39 +39,24 @@ public class RegisterActivity extends AppCompatActivity {
 
         usernameEditText = findViewById(R.id.username);
         passwordEditText = findViewById(R.id.password);
-        passwordRepeatEditText = findViewById(R.id.password2);  // Új jelszó ellenőrző mező
+        passwordRepeatEditText = findViewById(R.id.password2);
         signUpButton = findViewById(R.id.loginButton);
-        loginText = findViewById(R.id.signupText);
+        loginText = findViewById(R.id.loginText);  // Változás itt: signupText helyett loginText
 
-        signUpButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                registerUser();
-            }
-        });
+        signUpButton.setOnClickListener(v -> registerUser());
 
         loginText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
+                navigateToLogin();
             }
         });
+    }
 
-        usernameEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_DONE) {
-                    InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
-                    if (imm != null) {
-                        imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
-                    }
-                    return true;
-                }
-                return false;
-            }
-        });
+    private void navigateToLogin() {
+        Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 
     private void registerUser() {
@@ -87,46 +69,43 @@ public class RegisterActivity extends AppCompatActivity {
             return;
         }
 
-        // Ellenőrizzük, hogy a két jelszó egyezik-e
         if (!password.equals(repeatPassword)) {
             Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show();
             return;
         }
 
-        // Ellenőrizzük, hogy az email már létezik-e az adatbázisban
         mDatabase.child("users").orderByChild("email").equalTo(email)
                 .addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
                         if (dataSnapshot.exists()) {
-                            // Ha az email már létezik
-                            Toast.makeText(RegisterActivity.this, "Email already registered.", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(RegisterActivity.this, "Email already registered", Toast.LENGTH_SHORT).show();
                         } else {
-                            // Ha az email nem létezik, regisztrálhatunk
-                            mAuth.createUserWithEmailAndPassword(email, password)
-                                    .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
-                                        @Override
-                                        public void onComplete(@NonNull Task<AuthResult> task) {
-                                            if (task.isSuccessful()) {
-                                                Toast.makeText(RegisterActivity.this, "Registration Successful!", Toast.LENGTH_SHORT).show();
-                                                FirebaseUser user = mAuth.getCurrentUser();
-                                                saveUserToDatabase(user);
-                                                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
-                                                startActivity(intent);
-                                                finish();
-                                            } else {
-                                                Toast.makeText(RegisterActivity.this, "Registration failed: " + task.getException().getMessage(),
-                                                        Toast.LENGTH_SHORT).show();
-                                            }
-                                        }
-                                    });
+                            createUser(email, password);
                         }
                     }
 
                     @Override
                     public void onCancelled(DatabaseError databaseError) {
-                        // Hiba történt az adatbázis lekérdezése során
                         Toast.makeText(RegisterActivity.this, "Database error.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void createUser(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(RegisterActivity.this, "Registration Successful!", Toast.LENGTH_SHORT).show();
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            saveUserToDatabase(user);
+                            navigateToLogin();
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "Registration failed: " + task.getException().getMessage(),
+                                    Toast.LENGTH_SHORT).show();
+                        }
                     }
                 });
     }
